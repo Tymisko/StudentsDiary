0856d2a8f869abf1d4b6e2d1ae443cafdd48fa2d
Jan Urbaś <janurbas39@gmail.com>
2021-12-25 00:23:34 +0100

    Add AddEditStudent.cs form with basic user interface.

--- StudentsDiary/AddEditStudent.Designer.cs
+++ StudentsDiary/AddEditStudent.Designer.cs
@@ -32,266 +32,189 @@ namespace StudentsDiary
         private void InitializeComponent()
         {
             this.tbId = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labId = new System.Windows.Forms.Label();
             this.tbFirstName = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labFirstName = new System.Windows.Forms.Label();
             this.tbLastName = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labLastName = new System.Windows.Forms.Label();
             this.tbMath = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.labMath = new System.Windows.Forms.Label();
             this.tbTechnology = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.labTechnology = new System.Windows.Forms.Label();
             this.tbPhysics = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
+            this.labPhysics = new System.Windows.Forms.Label();
             this.tbPolishLang = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.tbForeignLang = new System.Windows.Forms.TextBox();
-            this.rtbComments = new System.Windows.Forms.RichTextBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.btnConfirm = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
+            this.labPolishLang = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.labForeign = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbId
             // 
-            this.tbId.Location = new System.Drawing.Point(120, 19);
+            this.tbId.Location = new System.Drawing.Point(107, 12);
             this.tbId.Name = "tbId";
             this.tbId.ReadOnly = true;
-            this.tbId.Size = new System.Drawing.Size(228, 20);
+            this.tbId.Size = new System.Drawing.Size(225, 20);
             this.tbId.TabIndex = 0;
-            this.tbId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label1
+            // labId
             // 
-            this.label1.Location = new System.Drawing.Point(14, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(14, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Id:";
-            this.label2.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(14, 48);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Imię:";
+            this.labId.Location = new System.Drawing.Point(30, 12);
+            this.labId.Name = "labId";
+            this.labId.Size = new System.Drawing.Size(71, 20);
+            this.labId.TabIndex = 1;
+            this.labId.Text = "Id";
             // 
             // tbFirstName
             // 
-            this.tbFirstName.Location = new System.Drawing.Point(120, 45);
+            this.tbFirstName.Location = new System.Drawing.Point(107, 38);
             this.tbFirstName.Name = "tbFirstName";
-            this.tbFirstName.Size = new System.Drawing.Size(228, 20);
-            this.tbFirstName.TabIndex = 2;
-            this.tbFirstName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.tbFirstName.Size = new System.Drawing.Size(225, 20);
+            this.tbFirstName.TabIndex = 0;
             // 
-            // label4
+            // labFirstName
             // 
-            this.label4.Location = new System.Drawing.Point(14, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 17);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Nazwisko:";
+            this.labFirstName.Location = new System.Drawing.Point(30, 38);
+            this.labFirstName.Name = "labFirstName";
+            this.labFirstName.Size = new System.Drawing.Size(71, 20);
+            this.labFirstName.TabIndex = 1;
+            this.labFirstName.Text = "Imię:";
             // 
             // tbLastName
             // 
-            this.tbLastName.Location = new System.Drawing.Point(120, 71);
+            this.tbLastName.Location = new System.Drawing.Point(107, 64);
             this.tbLastName.Name = "tbLastName";
-            this.tbLastName.Size = new System.Drawing.Size(228, 20);
-            this.tbLastName.TabIndex = 4;
+            this.tbLastName.Size = new System.Drawing.Size(225, 20);
+            this.tbLastName.TabIndex = 0;
             // 
-            // label5
+            // labLastName
             // 
-            this.label5.Location = new System.Drawing.Point(14, 100);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 17);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Matematyka:";
+            this.labLastName.Location = new System.Drawing.Point(30, 64);
+            this.labLastName.Name = "labLastName";
+            this.labLastName.Size = new System.Drawing.Size(71, 20);
+            this.labLastName.TabIndex = 1;
+            this.labLastName.Text = "Nazwisko:";
             // 
             // tbMath
             // 
-            this.tbMath.Location = new System.Drawing.Point(120, 97);
+            this.tbMath.Location = new System.Drawing.Point(107, 90);
             this.tbMath.Name = "tbMath";
-            this.tbMath.Size = new System.Drawing.Size(228, 20);
-            this.tbMath.TabIndex = 6;
+            this.tbMath.Size = new System.Drawing.Size(225, 20);
+            this.tbMath.TabIndex = 0;
             // 
-            // label6
+            // labMath
             // 
-            this.label6.Location = new System.Drawing.Point(14, 126);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(100, 17);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Technologia:";
+            this.labMath.Location = new System.Drawing.Point(30, 90);
+            this.labMath.Name = "labMath";
+            this.labMath.Size = new System.Drawing.Size(71, 20);
+            this.labMath.TabIndex = 1;
+            this.labMath.Text = "Matematyka:";
             // 
             // tbTechnology
             // 
-            this.tbTechnology.Location = new System.Drawing.Point(120, 123);
+            this.tbTechnology.Location = new System.Drawing.Point(107, 116);
             this.tbTechnology.Name = "tbTechnology";
-            this.tbTechnology.Size = new System.Drawing.Size(228, 20);
-            this.tbTechnology.TabIndex = 8;
+            this.tbTechnology.Size = new System.Drawing.Size(225, 20);
+            this.tbTechnology.TabIndex = 0;
             // 
-            // label7
+            // labTechnology
             // 
-            this.label7.Location = new System.Drawing.Point(14, 152);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 17);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Fizyka:";
+            this.labTechnology.Location = new System.Drawing.Point(30, 116);
+            this.labTechnology.Name = "labTechnology";
+            this.labTechnology.Size = new System.Drawing.Size(71, 20);
+            this.labTechnology.TabIndex = 1;
+            this.labTechnology.Text = "Technologia:";
             // 
             // tbPhysics
             // 
-            this.tbPhysics.Location = new System.Drawing.Point(120, 149);
+            this.tbPhysics.Location = new System.Drawing.Point(107, 142);
             this.tbPhysics.Name = "tbPhysics";
-            this.tbPhysics.Size = new System.Drawing.Size(228, 20);
-            this.tbPhysics.TabIndex = 10;
+            this.tbPhysics.Size = new System.Drawing.Size(225, 20);
+            this.tbPhysics.TabIndex = 0;
             // 
-            // label8
+            // labPhysics
             // 
-            this.label8.Location = new System.Drawing.Point(14, 178);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(100, 17);
-            this.label8.TabIndex = 13;
-            this.label8.Text = "Jęz pol.:";
+            this.labPhysics.Location = new System.Drawing.Point(30, 142);
+            this.labPhysics.Name = "labPhysics";
+            this.labPhysics.Size = new System.Drawing.Size(71, 20);
+            this.labPhysics.TabIndex = 1;
+            this.labPhysics.Text = "Fizyka:";
             // 
             // tbPolishLang
             // 
-            this.tbPolishLang.Location = new System.Drawing.Point(120, 175);
+            this.tbPolishLang.Location = new System.Drawing.Point(107, 168);
             this.tbPolishLang.Name = "tbPolishLang";
-            this.tbPolishLang.Size = new System.Drawing.Size(228, 20);
-            this.tbPolishLang.TabIndex = 12;
+            this.tbPolishLang.Size = new System.Drawing.Size(225, 20);
+            this.tbPolishLang.TabIndex = 0;
             // 
-            // label9
+            // labPolishLang
             // 
-            this.label9.Location = new System.Drawing.Point(14, 204);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(100, 17);
-            this.label9.TabIndex = 15;
-            this.label9.Text = "Jęz obc.:";
+            this.labPolishLang.Location = new System.Drawing.Point(30, 168);
+            this.labPolishLang.Name = "labPolishLang";
+            this.labPolishLang.Size = new System.Drawing.Size(71, 20);
+            this.labPolishLang.TabIndex = 1;
+            this.labPolishLang.Text = "Jęz. pol.";
             // 
-            // tbForeignLang
+            // textBox8
             // 
-            this.tbForeignLang.Location = new System.Drawing.Point(120, 201);
-            this.tbForeignLang.Name = "tbForeignLang";
-            this.tbForeignLang.Size = new System.Drawing.Size(228, 20);
-            this.tbForeignLang.TabIndex = 14;
+            this.textBox8.Location = new System.Drawing.Point(107, 194);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(225, 20);
+            this.textBox8.TabIndex = 0;
             // 
-            // rtbComments
+            // labForeign
             // 
-            this.rtbComments.Location = new System.Drawing.Point(122, 227);
-            this.rtbComments.Name = "rtbComments";
-            this.rtbComments.Size = new System.Drawing.Size(226, 102);
-            this.rtbComments.TabIndex = 16;
-            this.rtbComments.Text = "";
-            // 
-            // label10
-            // 
-            this.label10.Location = new System.Drawing.Point(14, 230);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(100, 17);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "Uwagi:";
-            this.label10.Click += new System.EventHandler(this.label10_Click);
-            // 
-            // btnConfirm
-            // 
-            this.btnConfirm.Location = new System.Drawing.Point(265, 345);
-            this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(83, 24);
-            this.btnConfirm.TabIndex = 18;
-            this.btnConfirm.Text = "Zatwierdź";
-            this.btnConfirm.UseVisualStyleBackColor = true;
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(176, 345);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(83, 24);
-            this.btnCancel.TabIndex = 19;
-            this.btnCancel.Text = "Anuluj";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.labForeign.Location = new System.Drawing.Point(30, 194);
+            this.labForeign.Name = "labForeign";
+            this.labForeign.Size = new System.Drawing.Size(71, 20);
+            this.labForeign.TabIndex = 1;
+            this.labForeign.Text = "Jęz. obc.:";
             // 
             // AddEditStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(370, 385);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnConfirm);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.rtbComments);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.tbForeignLang);
-            this.Controls.Add(this.label8);
+            this.ClientSize = new System.Drawing.Size(369, 450);
+            this.Controls.Add(this.labForeign);
+            this.Controls.Add(this.labPolishLang);
+            this.Controls.Add(this.labPhysics);
+            this.Controls.Add(this.labTechnology);
+            this.Controls.Add(this.labMath);
+            this.Controls.Add(this.labLastName);
+            this.Controls.Add(this.labFirstName);
+            this.Controls.Add(this.labId);
+            this.Controls.Add(this.textBox8);
             this.Controls.Add(this.tbPolishLang);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.tbPhysics);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.tbTechnology);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.tbMath);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.tbLastName);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.tbFirstName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.tbId);
-            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "AddEditStudent";
-            this.Load += new System.EventHandler(this.AddEditStudent_Load);
-            this.Click += new System.EventHandler(this.AddEditStudent_Click);
+            this.Text = "AddEditStudent";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label labId;
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox tbId;
-
-        private System.Windows.Forms.Button btnCancel;
-
-        private System.Windows.Forms.Button btnConfirm;
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-
-        private System.Windows.Forms.Label label10;
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
-
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox8;
-
         private System.Windows.Forms.Label label2;
-
-        private System.Windows.Forms.TextBox dtbId;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
 
         #endregion
     }
--- StudentsDiary/AddEditStudent.cs
+++ StudentsDiary/AddEditStudent.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace StudentsDiary
 {
@@ -8,26 +7,6 @@ namespace StudentsDiary
         public AddEditStudent()
         {
             InitializeComponent();
-        }
-
-        private void AddEditStudent_Load(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void AddEditStudent_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
